95f487729a41aed525f8ee85eaf2ee35effa9a20
Marquavious Draggon <marq.draggon@gmail.com>
2017-01-25 15:27:57 -0800

    progress

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
+
 var express = require('express')
 var app = express()
+var exphbs  = require('express-handlebars');
 
-var myLogger = function(req,res,next){
-    res.send('LOGGED')
-    next()
-}
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-var requestTime = function(req,res,next){
-    req.requestTime = Date.now()
-    next()
-}
+app.get('/', function (req, res) {
+    res.render('home');
+});
 
-// app.use(myLogger)
-app.use(requestTime)
-app.get('/', function (req,res){
-    var responseText = 'Hello World!<br>'
-      responseText += '<small>Requested at: ' + req.requestTime + '</small>'
-      res.send(responseText)
-})
-
-
-/*
-app.post('/', function (req, res) {
-  res.send('Got a POST request')
-})
-app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user')
-})
-app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user')
-})
-*/
+app.get('/posts', function (req, res){
+    var post = [
+    { body: "the stuff" },
+    { body: "the stuff" },
+    { body: "the stuff" }
+  ]
+  res.render('posts', {posts:post})
+});
 
 app.listen(3000, function(){
     console.log('hello_world listening on port 3000!')
